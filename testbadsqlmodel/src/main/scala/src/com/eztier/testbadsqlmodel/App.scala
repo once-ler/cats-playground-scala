4363395f154c4bdca8d9ad9aab7e6f3b7c7b25ee
once-ler <htao@eztier.com>
2019-10-27 09:24:46 -0400

    Add doobie implementation of infrastructure

--- testbadsqlmodel/src/main/scala/src/com/eztier/testbadsqlmodel/App.scala
+++ testbadsqlmodel/src/main/scala/src/com/eztier/testbadsqlmodel/App.scala
@@ -1,5 +1,12 @@
 package src.com.eztier.testbadsqlmodel
 
-class App {
+import cats.effect.{Async, ContextShift, IOApp}
+import io.circe.config.{parser => ConfigParser}
+
+object App extends IOApp {
+
+  def setupDatabase[F[_]: Async: ContextShift] = {
+
+  }
 
 }
--- testbadsqlmodel/src/main/scala/src/com/eztier/testbadsqlmodel/infrastructure/repository/doobie/DoobieTrialRepositoryInterpreter.scala
+++ testbadsqlmodel/src/main/scala/src/com/eztier/testbadsqlmodel/infrastructure/repository/doobie/DoobieTrialRepositoryInterpreter.scala
@@ -1,13 +1,14 @@
 package src.com.eztier.testbadsqlmodel
 package infrastructure.repository.doobie
 
+import cats.data.OptionT
+import cats.effect.Bracket
 import doobie._
 import doobie.implicits._
-
 import domain.trials._
 
 private object TrialSql {
-  def select(id: Long): Query0[Trial] = sql"""
+  def get(id: Long): Query0[Trial] = sql"""
     SELECT id, name, trial_arm_set
     FROM trial
     WHERE id = $id
@@ -15,7 +16,7 @@ private object TrialSql {
 }
 
 private object TrialContractSql {
-  def select(id: Long): Query0[TrialContract] = sql"""
+  def get(id: Long): Query0[TrialContract] = sql"""
     SELECT id, name, trial_id, fixed_general_item_set, fixed_personnel_item_set, indirect_rate, cost_per_participant
     FROM trial_contract
     WHERE id = $id
@@ -23,7 +24,7 @@ private object TrialContractSql {
 }
 
 private object TrialArmSql {
-  def select(id: Long): Query0[TrialArm] = sql"""
+  def get(id: Long): Query0[TrialArm] = sql"""
     SELECT id, name, num_participants, variable_procedure_item_set, variable_general_item_set
     FROM trial_arm
     WHERE id = $id
@@ -31,14 +32,34 @@ private object TrialArmSql {
 }
 
 private object JunctionSql {
-  def select(id: Long): Query0[Junction] = sql"""
+  def list(id: Long): Query0[Junction] = sql"""
     SELECT set_id, item_id
     FROM junction
     WHERE set_id = $id
   """.query
 }
 
-class DoobieTrialRepositoryInterpreter {
+class DoobieTrialRepositoryInterpreter[F[_]: Bracket[?[_], Throwable]](val xa: Transactor[F])
+  extends TrialRepositoryAlgebra[F] {
 
+  // OptionT[F, Trial] == F[Option[Trial]]
+  override def get(id: Long): OptionT[F, Trial] = OptionT(TrialSql.get(id).option.transact(xa))
+}
 
+class DoobieTrialArmRepositoryInterpreter[F[_]: Bracket[?[_], Throwable]](val xa: Transactor[F])
+  extends TrialArmRepositoryAlgebra[F] {
+
+  override def get(id: Long): OptionT[F, TrialArm] = OptionT(TrialArmSql.get(id).option.transact(xa))
+}
+
+class DoobieTrialContractRepositoryInterpreter[F[_]: Bracket[?[_], Throwable]](val xa: Transactor[F])
+  extends TrialContractRepositoryAlgebra[F] {
+
+  override def get(id: Long): OptionT[F, TrialContract] = OptionT(TrialContractSql.get(id).option.transact(xa))
+}
+
+class DoobieJunctionRepositoryInterpreter[F[_]: Bracket[?[_], Throwable]](val xa: Transactor[F])
+  extends JunctionRepositoryAlgebra[F] {
+
+  override def list(id: Long): F[List[Junction]] = JunctionSql.list(id).to[List].transact(xa)
 }
