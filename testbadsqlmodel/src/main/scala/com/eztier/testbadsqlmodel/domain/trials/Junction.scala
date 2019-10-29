e430f7d6345b8a1e22804d8f74823e80deec2b2a
once-ler <htao@eztier.com>
2019-10-29 18:51:39 -0400

    Drill into TrialAggregator

--- testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/Junction.scala
+++ testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/Junction.scala
@@ -1,6 +1,8 @@
 package com.eztier.testbadsqlmodel
 package domain.trials
 
+import cats.{Applicative, Functor}
+
 case class Junction
 (
   setId: Long,
@@ -9,4 +11,23 @@ case class Junction
 
 trait JunctionRepositoryAlgebra[F[_]] {
   def list(id: Long): F[List[Junction]]
+  def list(id: Option[Long]): F[List[Junction]]
+}
+
+class JunctionService[F[_]: Applicative](
+  repository: JunctionRepositoryAlgebra[F]
+) {
+  def list(id: Long): F[List[Junction]] = repository.list(id)
+
+  def list(id: Option[Long]): F[List[Junction]] =
+    id match {
+      case Some(a) => repository.list(a)
+      case None => Applicative[F].pure(List[Junction]())
+    }
+}
+
+object JunctionService {
+  def apply[F[_]: Applicative](
+    repository: JunctionRepositoryAlgebra[F]
+  ): JunctionService[F] = new JunctionService[F](repository)
 }
--- testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/TrialAggregator.scala
+++ testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/TrialAggregator.scala
@@ -1,36 +1,37 @@
 package com.eztier.testbadsqlmodel
 package domain.trials
 
-import cats.Functor
+import cats.{Applicative, Functor}
 import cats.data.EitherT
-import cats.effect.Sync
-import cats._
+import cats.effect.{IO, Sync}
+import cats.syntax._
 
-class TrialAggregator[F[_]: Functor : Sync](trialService: TrialService[F], trialArmService: TrialArmService[F], trialContractService: TrialContractService[F]) {
+import scala.concurrent.Future
+
+class TrialAggregator[F[_]: Applicative : Sync](trialService: TrialService[F], trialArmService: TrialArmService[F], trialContractService: TrialContractService[F], junctionService: JunctionService[F]) {
 
   // EitherT[F, A, B] == F[Either[A, B]]
   def run() = {
 
-    /*
+
     trialContractService.get(1).map {
       a =>
         trialService.exists(a.trialId).map {
           b =>
-            b.trialArmSet
+            val fc: F[List[Junction]] = junctionService.list(b.trialArmSet)
+            Functor[F].map(fc){
+              c =>
+                val fc1 = Applicative[F].pure(c)
+            }
         }
     }
-    */
 
-    for {
-      a <- trialContractService.get(1)
-      b <- trialService.exists(a.trialId)
-      c <- Functor[Long].map(trialArmService.get)(b.trialArmSet)
-    } yield ???
-
+    // https://medium.com/@scalaisfun/optiont-and-eithert-in-scala-90241aba1bb7
+    
   }
 }
 
 object TrialAggregator {
-  def apply[F[_]: Functor : Sync](trialService: TrialService[F], trialArmService: TrialArmService[F], trialContractService: TrialContractService[F]) =
-    new TrialAggregator[F](trialService, trialArmService, trialContractService)
+  def apply[F[_]: Functor : Sync](trialService: TrialService[F], trialArmService: TrialArmService[F], trialContractService: TrialContractService[F], junctionService: JunctionService[F]) =
+    new TrialAggregator[F](trialService, trialArmService, trialContractService, junctionService)
 }
