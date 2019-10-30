910a3a2437123b5b242153e53cf25b0c2d681762
once-ler <htao@eztier.com>
2019-10-30 19:18:16 -0400

    Use for comprehension for EitherT flow

--- testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/TrialAggregator.scala
+++ testbadsqlmodel/src/main/scala/com/eztier/testbadsqlmodel/domain/trials/TrialAggregator.scala
@@ -1,33 +1,52 @@
 package com.eztier.testbadsqlmodel
 package domain.trials
 
+import java.util.concurrent.Executors
+
 import cats.{Applicative, Functor}
 import cats.data.EitherT
 import cats.effect.{IO, Sync}
-import cats.syntax._
+import fs2.Stream
+
+import scala.concurrent.ExecutionContext
+// import cats.syntax._
+import cats.implicits._
+import cats.effect._
 
 import scala.concurrent.Future
+
+import domain.ValidationError
 
 class TrialAggregator[F[_]: Applicative : Sync](trialService: TrialService[F], trialArmService: TrialArmService[F], trialContractService: TrialContractService[F], junctionService: JunctionService[F]) {
 
   // EitherT[F, A, B] == F[Either[A, B]]
-  def run() = {
+  def run[F: Concurrent[?[_]]]()  = {
+    // https://medium.com/@scalaisfun/optiont-and-eithert-in-scala-90241aba1bb7
 
+    val action = for {
+      a <- trialContractService.get(1)
+      b <- trialService.exists(a.trialId)
+      c <- EitherT.rightT(b.trialArmSet)
+      d <- EitherT.liftF(junctionService.list(c))
+    } yield d
 
-    trialContractService.get(1).map {
-      a =>
-        trialService.exists(a.trialId).map {
-          b =>
-            val fc: F[List[Junction]] = junctionService.list(b.trialArmSet)
-            Functor[F].map(fc){
-              c =>
-                val fc1 = Applicative[F].pure(c)
-            }
-        }
+    val maybeItems = action.value.map {
+      case Right(list) => list
+      case Left(_) => List[Junction]()
     }
 
-    // https://medium.com/@scalaisfun/optiont-and-eithert-in-scala-90241aba1bb7
-    
+    implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+    implicit val concurrent = IO.ioConcurrentEffect(IO.contextShift(ec))
+    val concurrency = 4
+    // def eval[F[_],A](f: F[A]): Stream[F,A]
+    val computations = Stream.eval(maybeItems).map(
+      _.map {
+        b =>
+          trialArmService.get(b.itemId).value
+      }
+    ).parJoin(concurrency)
+
+    computations.covary[F].showLinesStdOut
   }
 }
 
