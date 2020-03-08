5392935d5b37096d2b1aa49cfca7168157d41ceb
once-ler <htao@eztier.com>
2020-03-07 21:37:12 -0500

    Simple test for TypeTag

--- common/src/test/scala/com/eztier/test/common/TestClassAttributes.scala
+++ common/src/test/scala/com/eztier/test/common/TestClassAttributes.scala
@@ -5,18 +5,40 @@ import org.specs2.mutable._
 import common._
 import Util._
 
+case class User(id: Long, firstName: String, lastName: String, role: String)
+
 class TestClassAttributes extends Specification {
+
+  import scala.reflect.runtime.universe.TypeTag
+
+  def toCkTypeName[A](implicit typeTag: TypeTag[A]) = {
+    val tname = typeTag.tpe.getClass.getSimpleName
+    tname match {
+      case a if a == "CkPostalContactInformation" => "Postal Contact Information"
+      case a if a == "CkPhoneContactInformation" => "Phone Contact Information"
+      case a if a == "CkEmailContactInformation" => "E-mail Contact Information"
+      case _ => tname.replace("Ck", "")
+    }
+  }
 
   "Case class attributes" should {
     "Get extracted" in {
-       case class User(id: Long, firstName: String, lastName: String, role: String)
+      val keys = Attributes[User].fieldNames
 
-       val keys = Attributes[User].fieldNames
+      val keys2 = getCCFieldNames[User]
 
-       val keys2 = getCCFieldNames[User]
-
-       keys mustEqual keys2
+      keys mustEqual keys2
     }
- }
+  }
+
+  "Type" should {
+    "Convert to simple string" in {
+      val tname = TypeInfo[User].name
+
+      val uname = toCkTypeName[User]
+
+      tname mustEqual uname
+    }
+  }
 
 }
